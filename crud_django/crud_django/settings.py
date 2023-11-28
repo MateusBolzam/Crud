f76e556e4e9a16e6bb0df2a7f469863aef20733d
MateusBolzam <mateusbolzam@gmail.com>
2023-11-28 09:55:26 -0300

    alguns ajustes com bootsrap

--- crud_django/crud_django/settings.py
+++ crud_django/crud_django/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'crudsimples',
         'USER': 'postgres',
-        'PASSWORD': ' ',
+        'PASSWORD': '0511',
         'HOST': 'localhost',
         'PORT': '',  # Opcional: você pode especificar a porta se não for a padrão
     }
